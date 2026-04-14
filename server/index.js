33e461329d8b91a00f6c5258dc14a63d80352cb9
Santhosh9600 <148633650+Santhosh9600@users.noreply.github.com>
2026-04-14 16:12:45 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -102,7 +102,7 @@ app.delete("/api/orders/:id", async (req, res) => {
 });
 
 /* ---------------- SERVER START ---------------- */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
